9e4f95892ff786d9373956c2c5c4086b1d6b6ee1
emekaenshinyan <emeka.enshinyan@gmail.com>
2024-05-15 14:25:52 +1000

    edit: t1

--- task1/runner.py
+++ task1/runner.py
@@ -1,28 +1,52 @@
+'''PURPOSE'''
+#Create a structure to hold information about 
+    # races and process racing data
+
 from custom_errors import *
 
 class Runner:
-    def __init__(self, name, age, country, sprint_speed, endurance_speed):
+    def __init__(self, name: str, age: int, country, sprint_speed: float, endurance_speed: float):
         max_energy = 1000
         self.name = name
+        # between 5-20 inclusive
         self.age = age
+        # from the .csv file
         self.country = country
+        # between 2.2-6.8 inclusive
         self.sprint_speed = sprint_speed
+        # between 1.8-5.4 meters per second inclusive
         self.endurance_speed = endurance_speed
+        # current level of energy
         self.energy = max_energy
     
-    def drain_energy(self, drain_points):
+    
+    # return the value of energy and check the value's params
+    def drain_energy(self, drain_points) -> int:
+        assert self.energy < 0, "energy cannot be less than 0"
+        assert drain_points < 0, "drain_points cannot be less than 0"
+        assert drain_points > self.energy, "drain_points cannot be higher than max energy"
         self.energy -= drain_points
+        assert self.energy < 0, "energy cannot be less than 0"
+        return self.energy
     
+    # check recovery value's params
     def recover_energy(self, recovery_amount):
+        assert recovery_amount > 1000, "cannot be over 1000"
         self.energy += recovery_amount
-    
-    def run_race(self, race_type, distance):
-        race_type = 'short'
-        return distance * self.sprint_speed
+        if self.energy > 1000:
+            self.energy = 1000
+        
+    # make a dict to st race type and distance    
+    def run_race(self, race_type, distance) -> float:
+        race_dict = {'race_type': race_type, 'distance': distance}        
+        return race_dict['distance'] * self.sprint_speed
     
     def __str__(self):
         return f"Name: {self.name} Age: {self.age} Country: {self.country}"
 
+    def valid_inputs(self):
+        pass
+    
 if __name__ == '__main__':
     runner = Runner('Elijah', 18, 'Australia', 5.8, 4.4)
     
